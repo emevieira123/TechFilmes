6ab3d88c909cf295fa59f78d9619a5eeefb4347d
Emerson Vieira <emevieira@gmail.com>
2023-04-21 14:44:06 -0400

    Adicionado analystic Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { GlobalStyle } from './styles/global'
 import { QueryClientProvider, QueryClient } from 'react-query'
+import { Analytics } from '@vercel/analytics/react'
 
 export const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <QueryClientProvider client={queryClient}>
       <App />
       <GlobalStyle />
+      <Analytics />
     </QueryClientProvider>
   </React.StrictMode>,
 )
