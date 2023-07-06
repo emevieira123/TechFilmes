3c7e9d3b998b0903e752ae8c2ed41e860611084a
Emerson Vieira <33058881+emevieira123@users.noreply.github.com>
2023-07-06 13:29:09 -0400

    Alteracao dos banners do slide da tela inicial

--- src/app/HomePage/__mocks__/bannerMock.ts
+++ src/app/HomePage/__mocks__/bannerMock.ts
@@ -1,17 +1,17 @@
 export const bannerMock = [
   {
     id: 1,
-    title: 'Avatar 2',
-    imgURL: 'https://thegeekstation.files.wordpress.com/2015/01/banner-avatar.jpg?w=772'
+    title: 'Velozes e Furiosos X',
+    imgURL: 'https://mundoconectado.com.br/uploads/chamadas/23_05_15_091225.jpg'
   },
   {
     id: 2,
-    title: 'Adão Negro',
-    imgURL: 'https://imageup.me/images/1fb764da-4572-4d1d-b677-37d1219d5765.png'
+    title: 'Transformers: O Despertar da Feras',
+    imgURL: 'https://ovicio.com.br/wp-content/uploads/2022/10/20221018-ovicio-transformers-rise.webp'
   },
   {
     id: 3,
-    title: 'Wakanda Forever',
-    imgURL: 'https://imageup.me/images/9be8e92b-ad64-4873-b292-4baa5b010348.png'
+    title: 'Homem Aranha No Aranhaverso',
+    imgURL: 'https://cinepop.com.br/wp-content/uploads/2023/01/homem-aranha-atraves-do-aranhaverso.jpg'
   }
 ]
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { GlobalStyle } from './styles/global'
 import { QueryClientProvider, QueryClient } from 'react-query'
-import { Analytics } from '@vercel/analytics/react'
 
 export const queryClient = new QueryClient();
 
@@ -12,7 +11,6 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <QueryClientProvider client={queryClient}>
       <App />
       <GlobalStyle />
-      <Analytics />
     </QueryClientProvider>
   </React.StrictMode>,
 )
