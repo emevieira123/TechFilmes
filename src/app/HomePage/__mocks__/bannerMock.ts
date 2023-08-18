7c43a6f7c02d69dee3f7c4d963b941ca0a71604a
Emerson Vieira <33058881+emevieira123@users.noreply.github.com>
2023-08-18 12:16:43 -0400

    Teste de mapeamento de calor da Clarity

--- src/app/HomePage/__mocks__/bannerMock.ts
+++ src/app/HomePage/__mocks__/bannerMock.ts
@@ -1,17 +1,17 @@
 export const bannerMock = [
   {
     id: 1,
-    title: 'Velozes e Furiosos X',
-    imgURL: 'https://mundoconectado.com.br/uploads/chamadas/23_05_15_091225.jpg'
+    title: 'OPPENHEIMER',
+    imgURL: 'https://preview.redd.it/y9ocptgp6zc91.jpg?width=1500&format=pjpg&auto=webp&s=2fe63d41e73ec1edb82e147bd6d2feebdf6b386b'
   },
   {
     id: 2,
-    title: 'Transformers: O Despertar da Feras',
-    imgURL: 'https://ovicio.com.br/wp-content/uploads/2022/10/20221018-ovicio-transformers-rise.webp'
+    title: 'MEGATUBARÃO 2',
+    imgURL: 'https://i.ytimg.com/vi/vFy_ZbgSef0/hq720.jpg?sqp=-oaymwEXCK4FEIIDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLCs9ySgPdr6rIdGGL3c3LF8q6XMSg'
   },
   {
     id: 3,
-    title: 'Homem Aranha No Aranhaverso',
-    imgURL: 'https://cinepop.com.br/wp-content/uploads/2023/01/homem-aranha-atraves-do-aranhaverso.jpg'
+    title: 'Besouro Azul',
+    imgURL: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiKbPq0nDiyrCc4aaNiK0mv1sNYPl2azZ2yFeYwCa31acyFodu0Bm6oKY7va04dtFWESG5kldFJt1DvVVLrYBIVPKpWQTC7DjO0uktTUmJv3gAGtlg3JcA_Y-pgioDytz0BL7kQe0qVTDw_dO8W_eS1nxL51YAcqlC5u7dM6yOgdKYAqSf9k2Ul5qJ7hQ/s1419/ba1.jpg'
   }
 ]
